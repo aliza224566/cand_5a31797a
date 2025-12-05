68281343a1ace3ff755e927b6ed1074048aefdab
aliza224566 <alizaak265@gmail.com>
2025-12-05 17:32:54 +0500

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,26 +1,20 @@
-import math
+from collections import Counter
 
-def knn_predict(train_X, train_y, test_X, k):
+def knn_predict(train_X, train_y, test_X, k) -> list[int]:
+    predictions = []
+    for test_point in test_X:
+        # Compute distances
+        dists = [(math.sqrt(sum((tx - trx) ** 2 for tx, trx in zip(test_point, train_point))), label)
+                 for train_point, label in zip(train_X, train_y)]
+        # Sort by distance, then label
+        dists.sort(key=lambda x: (x[0], x[1]))
+        # Take k nearest
+        nearest_labels = [label for _, label in dists[:k]]
+        # Majority vote
+        count = Counter(nearest_labels)
+        max_votes = max(count.values())
+        # Break tie by smallest label
+        pred_label = min(label for label, votes in count.items() if votes == max_votes)
+        predictions.append(pred_label)
+    return predictions
 
-    def dist(a, b):
-        return math.sqrt(sum((a[i] - b[i]) ** 2 for i in range(len(a))))
-
-    preds = []
-
-    for x in test_X:
-        dlist = []
-        for tx, lbl in zip(train_X, train_y):
-            dlist.append((dist(x, tx), lbl))
-
-        dlist.sort(key=lambda t: (t[0], t[1]))
-
-        kn = dlist[:k]
-
-        freq = {}
-        for _, lbl in kn:
-            freq[lbl] = freq.get(lbl, 0) + 1
-
-        best_label = max(freq.keys(), key=lambda lbl: (freq[lbl], -lbl))
-        preds.append(best_label)
-
-    return preds
